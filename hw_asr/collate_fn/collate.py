audio length fix for SiSDR masking

hw_asr/collate_fn/collate.py
@@ -32,6 +32,10 @@ def collate_fn(dataset_items: List[dict]):
         lengths["ref"].append(item["ref_audio"].shape[-1])
         lengths["input"].append(item["mix_audio"].shape[-1])
         lengths["target"].append(item["target_audio"].shape[-1])    
+
+    audio_length = dataset_items[0]["audio_length"]
+    audio_lengths = torch.tensor([audio_length], dtype=torch.int32)
+    audio_lengths = audio_lengths.repeat(len(dataset_items))
     
     return {
         "input": pad_sequence(audios["input"], batch_first=True),
@@ -40,5 +44,6 @@ def collate_fn(dataset_items: List[dict]):
         "input_length": torch.tensor(lengths["input"], dtype=torch.int32),
         "ref_length": torch.tensor(lengths["ref"], dtype=torch.int32),
         "target_length": torch.tensor(lengths["target"], dtype=torch.int32), 
-        "speaker_id": torch.tensor(speaker_ids, dtype=torch.int32)
+        "speaker_id": torch.tensor(speaker_ids, dtype=torch.int32),
+        "audio_length": audio_lengths
     }

hw_asr/loss/SpExPlusLoss.py
@@ -34,17 +34,16 @@ class SpExPlusLoss(_Loss):
 
 
     def forward(self, predicts, target, speaker_logits, speaker_id, 
-                input_length, target_length, **batch) -> Tuple[Tensor, Tensor]:
+                audio_length, **batch) -> Tuple[Tensor, Tensor]:
         """
         predicts: dict with predicts by filters "L1", "L2", "L3" 
         target: target audio
         speaker_logits: speaker encoder logits for Cross-Entropy Loss
         speaker_id: target speaker id for Cross-Entropy Loss
-        input_length: length of input audio
-        target_length: length of target audio
+        audio_length: mixed audio length from dataset, used for masking
         """
         sisdr_losses = {}
-        mask = torch.arange(target.shape[1])[None, :] < input_length[:, None]
+        mask = torch.arange(target.shape[1])[None, :] < audio_length[:, None]
         target = target[mask]
         
         for filter, predict in predicts.items():